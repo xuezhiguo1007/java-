5e78fd1118456b891096dee3f9c1b7efc74c269e
xuezhiguo <1461680129@qq.com>
2018-12-02 16:57:03 +0800

    Update javamysql.java

--- javamysql.java
+++ javamysql.java
@@ -1,5 +1,10 @@
 package com.runoob.test;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MySQLDemo {
 	//JDBC驱动及数据库URL
 	static final String JDBC_DRIVER="com.mysql.jdbc.Driver";
@@ -18,7 +23,45 @@ public class MySQLDemo {
 			conn=DriverManager.getConnection(DB_URL,USER,PASS);
 			//执行查询
 			System.out.println("实例化Statement对象...");
-			stmt=conn
-		}
+			stmt=conn.createStatement();
+			String sql;
+			sql="SELECT id,name,url FROM websites";
+			ResultSet rs=stmt.executeQuery(sql);
+			//展开结果集数据库
+			while(rs.next()) {
+				//通过字段检索
+				int id=rs.getInt("id");
+				String name=rs.getString("name");
+				String url=rs.getString("url");
+				//输出数据
+				System.out.print("ID:"+id);
+				System.out.print(", 站点名称: " + name);
+                System.out.print(", 站点 URL: " + url);
+                System.out.print("\n");
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+		}catch(SQLException se) {
+			//处理JDBC错误
+			se.printStackTrace();
+		 }catch(Exception e){
+	            // 处理 Class.forName 错误
+	            e.printStackTrace();
+	        }finally{
+	            // 关闭资源
+	            try{
+	                if(stmt!=null) stmt.close();
+	            }catch(SQLException se2){
+	            }// 什么都不做
+	            try{
+	                if(conn!=null) conn.close();
+	            }catch(SQLException se){
+	                se.printStackTrace();
+	            }
+	        }
+	        System.out.println("Goodbye!");
+	    }
 	}
-}
+		
+	
